ae558aab5e2dbdb7037610ccc2e2e3385229ce24
Andrii Klymeniuk <andrii.klymeniuk@fordewind.io>
2022-01-04 00:07:45 +0200

    Move: graphql example to Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,32 @@
-import * as React from "react"
-import { Link } from 'gatsby';
-import { Title } from './Layout.sc';
+import * as React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import { Title } from "./Layout.sc";
 
 const Layout = ({ pageTitle, size, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <div>
+      <title>
+        {pageTitle} | {data.site.siteMetadata.title}
+      </title>
       <Title size={size}>{pageTitle}</Title>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
         </ul>
       </nav>
       <main>
@@ -17,6 +34,6 @@ const Layout = ({ pageTitle, size, children }) => {
         {children}
       </main>
     </div>
-  )
-}
+  );
+};
 export default Layout;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,32 +1,23 @@
-import * as React from "react"
-import { Link, useStaticQuery, graphql } from 'gatsby';
-import Layout from '../components/Layout/Layout';
-import { StaticImage } from 'gatsby-plugin-image';
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/Layout/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const AboutPage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
   return (
     <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
       <Link to="/">Back to index page</Link>
       <br />
-      {data.site.siteMetadata.title}
       <hr />
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
       />
     </Layout>
-  )
-}
+  );
+};
 
 export default AboutPage;
